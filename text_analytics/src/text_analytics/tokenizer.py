c89a740319eaef3e97eff66455055d1d9ea601c7
timrleahy <102836735+timrleahy@users.noreply.github.com>
2026-03-05 14:47:37 -0600

    got rid of test case

--- text_analytics/src/text_analytics/tokenizer.py
+++ text_analytics/src/text_analytics/tokenizer.py
@@ -72,9 +72,3 @@ def remove_stopwords(words, stopwords=None):
         stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]    
     
     return [word for word in words if word not in stopwords]
-
-
-
-testString = "This is a test sentence, with punctuation and multiple spaces between some words. I'm adding a sentence with a stopword at the end me. The Dr. went to the river."
-testString = tokenize(testString)
-print(remove_stopwords(testString))
