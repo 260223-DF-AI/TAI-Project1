fa92d39fa648ed4f79f84a8905d13a85af74d499
timrleahy <102836735+timrleahy@users.noreply.github.com>
2026-03-05 14:44:29 -0600

    finished most functions in tokenizer

--- text_analytics/src/text_analytics/tokenizer.py
+++ text_analytics/src/text_analytics/tokenizer.py
@@ -1,5 +1,8 @@
 import re
 from collections import Counter
+# import nltk
+# nltk.download("punkt")
+# from nltk.tokenize import sent_tokenize
 
 def tokenize(text):
     """
@@ -9,7 +12,14 @@ def tokenize(text):
     - Remove extra whitespace
     Returns: List of words
     """
-    pass
+    wordList = []
+    text = text.lower()
+    text = re.sub(r"[^\w\s]", "", text)
+    text = text.strip()
+    wordList = text.split(" ")
+    wordList = [word for word in wordList if word != ""]
+    return wordList
+
 
 def get_sentences(text):
     """
@@ -18,7 +28,21 @@ def get_sentences(text):
     - Handle multiple punctuation (!! or ...)
     Returns: List of sentences
     """
-    pass
+    #\
+    sentenceList = []
+    sentenceList = text.split(".")
+    # #sentenceList = sent_tokenize(text)
+    # abbreviations = r'(Mr|Mrs|Ms|Dr|Prof|Rev|Jr|Sr|St|Mt)'
+    # domains = r'(com|org|net|edu|gov|co|io|uk|ca)'
+    
+    # pattern = r'(?<!Mr)(?<!Mrs)(?<!Ms)(?<!Dr)(?<!Prof)(?<!Rev)(?<!Jr)(?<!Sr)(?<!St)(?<!Mt)\.'
+
+    # #pattern = rf'(?s)(.*?)(?<!\d)(?<!\b{abbreviations})\.(?<!\d)(?<!\.(?:{domains}))[.!?](?:\s+|$)'
+    # sentenceList = re.findall(pattern, text)
+    
+    # return [sentence.strip() for sentence in sentenceList if sentence.strip()]
+    return sentenceList
+
 
 def get_ngrams(words, n):
     """
@@ -26,7 +50,17 @@ def get_ngrams(words, n):
     Example: get_ngrams(['a', 'b', 'c'], 2) -> [('a', 'b'), ('b', 'c')]
     Returns: List of tuples
     """
-    pass
+    ngramList = []
+    for i, word in enumerate(words):
+        if i == len(words) - n:
+            break
+        ngram = []
+        for j in range(n):
+            ngram.append(words[j + i])
+        ngramList.append(tuple(ngram))
+        #ngram.clear()
+    return ngramList
+
 
 def remove_stopwords(words, stopwords=None):
     """
@@ -34,4 +68,13 @@ def remove_stopwords(words, stopwords=None):
     Use a default set if stopwords not provided.
     Returns: Filtered list of words
     """
-    pass
+    if stopwords == None:
+        stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]    
+    
+    return [word for word in words if word not in stopwords]
+
+
+
+testString = "This is a test sentence, with punctuation and multiple spaces between some words. I'm adding a sentence with a stopword at the end me. The Dr. went to the river."
+testString = tokenize(testString)
+print(remove_stopwords(testString))
