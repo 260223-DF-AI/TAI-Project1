efb0bbd61d62d843f11360decb17cc40e16eed42
aziets399 <axz399@case.edu>
2026-03-09 11:42:45 -0500

    got test file to actually test code

--- DataIngestionSubsystem/Tests/test_.py
+++ DataIngestionSubsystem/Tests/test_.py
@@ -1,19 +1,22 @@
+import pandas as pd
 import pytest 
-import DataIngestionSubsystem.src.file_reader as file_reader
+from src.file_reader import load_data, clean_data, is_unique_column, find_empty_columns, compare_two_colums
 
 class TestFileReader:
 
     #Arrange/Act 
-    file = file_reader.load_data("/Users/andrewziets/Documents/Revature/TAI-Project1/DataIngestionSubsystem/data/sidewalk-cafe-permits.csv")
+    file = load_data("/Users/andrewziets/Documents/Revature/TAI-Project1/DataIngestionSubsystem/data/sidewalk-cafe-permits.csv")
 
-    def test_load_data():
+    def test_load_data(self):
         with pytest.raises(FileNotFoundError):
-            file_reader.load_data("")
+            load_data("")
 
         #Assert 
         assert file != None
 
-    def test_clean_data():
+    def test_clean_data(self):
+        # Duplicates Check
+
         # Arrange 
         data = {
             'Name': ['Alice', 'Bob', 'Alice', 'Charlie', 'Bob'],
@@ -21,15 +24,18 @@ class TestFileReader:
             'City': ['New York', 'Los Angeles', 'New York', 'Chicago', 'Los Angeles']
         }
         df = pd.DataFrame(data)
-        new_df = file_reader.clean_data(df)
+        new_df = clean_data(df)
         #Act 
         alice_count = (new_df['Name'] == 'Alice').sum()
         #Assert 
         assert alice_count == 1
 
-        #Assert 
+        #Assert - drop LOCATIONS and ADDRESS NUMBER
         assert (file["LOCATIONS"] == 0).all()
         assert (file["ADDRESS NUMBER START"] == 0).all()
+
+        #
+
 
 
 
