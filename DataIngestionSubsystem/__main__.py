18040cafea0e5626f9312f5d3869907e8150ece3
timrleahy <102836735+timrleahy@users.noreply.github.com>
2026-03-10 16:34:58 -0500

    Logger working, outputs to file

--- DataIngestionSubsystem/__main__.py
+++ DataIngestionSubsystem/__main__.py
@@ -1,13 +1,12 @@
 from DataIngestionSubsystem.src import *
 
 def main():
-    print("starting")
     logger = setup_logger(__name__)
     logger.info("Main started")
-
+    df = load_data("DataIngestionSubsystem/data/small-chunk.csv")
+    logger.info("data successfully loaded")
+    print(df)
     logger.info("Main completed successfully")
-    print("ending")
+
 if __name__ == "__main__":
     main()
-
-# main()
--- DataIngestionSubsystem/src/logger.py
+++ DataIngestionSubsystem/src/logger.py
@@ -1,15 +1,26 @@
 import logging
 
-def setup_logger(name):
+def setup_logger(name, logfile="app.log"):
     """Configure a custom logger."""
     logger = logging.getLogger(name)
+
     if not logger.handlers:
-        logger.setLevel(logging.WARNING)
-        handler = logging.StreamHandler()
+        logger.setLevel(logging.DEBUG)
+
         formatter = logging.Formatter(
             '%(asctime)s | %(name)s | %(levelname)s | %(message)s',
-            datefmt = '%H:%M:%S'
+            datefmt='%H:%M:%S'
         )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+
+        # Console handler
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        # File handler
+        file_handler = logging.FileHandler(logfile)
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(stream_handler)
+        logger.addHandler(file_handler)
+
     return logger
