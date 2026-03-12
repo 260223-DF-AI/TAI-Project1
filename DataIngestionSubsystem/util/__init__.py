13d409a3bca8e55d34c40d4128bbf1bbb807a71b
aziets399 <axz399@case.edu>
2026-03-12 14:51:34 -0500

    establshed requirements.txt

--- DataIngestionSubsystem/util/__init__.py
+++
@@ -1,7 +0,0 @@
-#Nothing here until exceptions and sql_connection are started
-# from .exceptions import 
-# from .sql_connection import 
-# __all__ = [
-#     "is_valid_email",
-#     "is_valid_age"
-# ]
